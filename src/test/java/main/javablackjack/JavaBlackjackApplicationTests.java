c103242491c0e0eaf91b3f30aa6d384e03950993
Ma-due <maduee2@naver.com>
2021-11-17 22:53:47 +0900

    feat(*) : build.gradle 업데이트

---
+++ src/test/java/main/javablackjack/JavaBlackjackApplicationTests.java
@@ -0,0 +1,13 @@
+package main.javablackjack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaBlackjackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
